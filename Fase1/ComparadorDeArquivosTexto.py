908afb8dc3b979855c81c186abf89a345c8bd0a9
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2020-07-27 20:18:09 -0300

    Update ComparadorDeArquivosTexto.py

--- Fase1/ComparadorDeArquivosTexto.py
+++ Fase1/ComparadorDeArquivosTexto.py
@@ -1,13 +1,12 @@
 texto = open('/home/itanu/Área de Trabalho/com_imp.txt', 'r')
 texto_copia = open('/home/itanu/Área de Trabalho/teste.txt', 'r')
-contador_iguais = total = 0
+contador_iguais = 0
 texto_1 = texto.read()
 texto_2 = texto_copia.read()
 for indice in range(0, 100):
     if texto_1[indice] == texto_2[indice]:
         contador_iguais += 1
-    total += 1
-print(f'Em uma amostra com {total} caracteres\n'
+print(f'Em uma amostra com 100 caracteres\n'
       f'Possui um total de {contador_iguais} caracteres iguais')
 texto.close()
 texto_copia.close()
