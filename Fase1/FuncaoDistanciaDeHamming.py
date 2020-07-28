58d869c8be84a1cb22368f17d3845870a1ea6624
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2020-07-27 21:47:30 -0300

    Update FuncaoDistanciaDeHamming.py

--- Fase1/FuncaoDistanciaDeHamming.py
+++ Fase1/FuncaoDistanciaDeHamming.py
@@ -5,11 +5,15 @@ r = (1 << np.arange(8))[:,None]
 return np.count_nonzero( (a & r) != (b & r) )
 
 def xor(a, b):
-    a = 10
-    b = 4
     c = (a ^ b)
-    print(c)
+    count_zero = 0
+    while c != 0:
+        if c % 2 == 0:
+            count_zero += 1
+        c //= 2
     
+        
+xor(10, 4)
 
 
 """
