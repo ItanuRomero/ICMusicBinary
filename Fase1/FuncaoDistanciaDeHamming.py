5396c31d5dbc56a0c5c3edd6178d6ad826a920af
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2020-07-27 21:38:04 -0300

    Update FuncaoDistanciaDeHamming.py

--- Fase1/FuncaoDistanciaDeHamming.py
+++ Fase1/FuncaoDistanciaDeHamming.py
@@ -4,9 +4,17 @@ def hamming_distance(a, b):
 r = (1 << np.arange(8))[:,None]
 return np.count_nonzero( (a & r) != (b & r) )
 
+def xor(a, b):
+    a = 10
+    b = 4
+    c = (a ^ b)
+    print(c)
+    
+
 
 """
 Link de onde foi retirado:
 https://living-sun.com/pt/python/702165-fastest-way-to-get-hamming-distance-for-integer-array-python-numpy-binary-xor-hamming-distance.html
 ainda precisamos testar.
+https://wiki.python.org.br/BitwiseOperators
 """
