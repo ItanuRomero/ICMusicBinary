4761e40171c1eb3f82e6fc8b77fdb64f6146ebec
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2020-11-10 17:08:04 -0300

    day 10-11

--- projetoDevCpp/hammingFunctions.c
+++ projetoDevCpp/hammingFunctions.c
@@ -28,10 +28,10 @@ float analyse_file()
 
         // Valor padrao para testes
         if (strcmp(name_1, "padrao") == 0) {
-            strcpy(name_1, "./testes/teste1.mid");
+            strcpy(name_1, "./testes/rap.mid");
         }
         if (strcmp(name_2, "padrao") == 0) {
-            strcpy(name_2, "./testes/teste2.mid");
+            strcpy(name_2, "./testes/original.mid");
         }
 
     // Faz leitura dos arquivos
